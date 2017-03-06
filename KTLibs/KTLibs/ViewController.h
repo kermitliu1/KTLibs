b9a27ada7b2de080a4d77fdb7dce42fb1b217e4a
KermitLiu <kermitkangxu@173.com>
2017-03-06 11:32:45 +0800

    Create Libs

---
+++ KTLibs/KTLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTLibs
+//
+//  Created by KermitLiu on 2017/3/6.
+//  Copyright © 2017年 KermitLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KTLibs/KTLibs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KTLibs
+//
+//  Created by KermitLiu on 2017/3/6.
+//  Copyright © 2017年 KermitLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
